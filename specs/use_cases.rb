c818ccb3dd61cfd81ba3157da6915091b5b48961
bohms <bohms@cd71fa47-3570-497c-b10c-88e95d1ea7d3>
2008-12-03 16:32:26 +0000

    move to trunk

---
+++ specs/use_cases.rb
@@ -0,0 +1,32 @@
+require 'cucumber'
+
+Feature %|Report
+  In order to produce a progress report to the nsf
+  as an information manager
+  I want to retrieve the demography of the investigators 
+      that were active during the reporting period
+
+end
+
+Feature %|Report
+  In order to produce a progress report the the nsf
+  as an information manger
+  I want to find all active datasets
+  
+end
+
+Feature %|Report
+  In order to produce a progress report the the nsf
+  as an information manger
+  I want to find all publications created during the reporting period
+end
+
+  In order to 
+  Scenario do
+    Given
+    and
+    when
+    Then
+    and
+  end
+end
